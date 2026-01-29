6ce898b7c28886e518ff221675704d7a2d0561c5
ggkchen <12345gavinchen@gmail.com>
2026-01-29 08:42:03 -0800

    add shooter and turret subsystems with control logic and configurations

---
+++ src/main/java/frc/robot/subsystems/turret/ShooterSubsystem.java
@@ -0,0 +1,198 @@
+package frc.robot.subsystems.turret;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private static final ShooterSubsystem INSTANCE = new ShooterSubsystem();
+
+  public static ShooterSubsystem getInstance() {
+    return INSTANCE;
+  }
+
+  public enum ShooterState {
+    IDLE,
+    SPIN_UP,
+    RAPID_FIRE,
+    RAPID_FIRE_ACCURATE,
+    EMPTY
+  }
+
+  private ShooterState state = ShooterState.IDLE;
+
+  private final DoubleSupplier currentTime = Timer::getFPGATimestamp;
+
+  static final double kS = 0.2; // static friction (volts)
+  static final double kV =
+      0.114; // volts per RPS //0.10475 to tune use dutycycleout run it to 70 rps and kV = voltage /
+  // 70 RPS
+  static final double kA = 0.001; // volts per RPS^2
+
+  static final double kP = 1.0;
+  static final double kI = 0.0;
+  static final double kD = 0.0;
+
+  double kP_BOOST = 2.0; // ~2x normal
+  double kD_BOOST = 0.002;
+
+  static final double kP_SPIN_UP = 0.05; // 0.01
+  static final double kI_SPIN_UP = 0.0; // 0.0001
+  static final double kD_SPIN_UP = 0.0002; // 0.0002
+
+  private final TalonFX shooterMotor = new TalonFX(1); // chjange ID
+  private static final double GEAR_RATIO = 1.0; // 1:1
+
+  private double timeAtSpeed = 0.0;
+  private double lastRPM = 0.0;
+  private boolean worked = false;
+  SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(kS, kV, kA);
+  PIDController velocityPID = new PIDController(kP, kI, kD);
+  PIDController spinUpPID = new PIDController(kP_SPIN_UP, kI_SPIN_UP, kD_SPIN_UP);
+
+  // config
+  private final double overspinFactor = 1.0;
+  private final double rpmRapidFireTolerance = 200;
+  private final double rpmAccurateTolerance = 50;
+  boolean shotRecentlyFired = false;
+  private double lastTargetRPS = 0.0;
+  private final VoltageOut voltageRequest = new VoltageOut(0);
+  private double lastShotTime = 0.0;
+
+  private static final InterpolatingDoubleTreeMap shotFlywheelSpeedMap =
+      new InterpolatingDoubleTreeMap();
+
+  static {
+    // this is example later replace
+    shotFlywheelSpeedMap.put(2.0, 4200.0); // its distance and second one is rpm
+  }
+
+  public ShooterSubsystem() {
+    var config = new TalonFXConfiguration();
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    config.CurrentLimits.SupplyCurrentLimit = 40.0;
+    config.CurrentLimits.SupplyCurrentLimitEnable = true;
+    shooterMotor.getConfigurator().apply(config);
+
+    SmartDashboard.putNumber("Shooter/kP", kP);
+    SmartDashboard.putNumber("Shooter/kD", kD);
+    SmartDashboard.putNumber("Shooter/kV", kV);
+  }
+
+  @Override
+  public void periodic() {
+    double tuneP = SmartDashboard.getNumber("Shooter/kP", kP); // test
+    double tuneD = SmartDashboard.getNumber("Shooter/kD", kD);
+
+    double distance = getTargetDistance(); // change
+
+    double baseRPM = shotFlywheelSpeedMap.get(distance);
+    double targetRPM = baseRPM * overspinFactor;
+    double currentRPM = getVelocityRevPerSec() * 60.0;
+
+    double targetRPS = targetRPM / 60.0;
+    double currentRPS = currentRPM / 60.0;
+
+    double rpmDrop = lastRPM - currentRPM;
+    //    shotRecentlyFired = rpmDrop > 150; // tune
+    double rpmSlope = (currentRPM - lastRPM) / 0.02;
+
+    if (rpmSlope < -7500) {
+      shotRecentlyFired = true;
+      lastShotTime = currentTime.getAsDouble();
+    }
+
+    if (shotRecentlyFired && ((currentTime.getAsDouble() - lastShotTime) > 0.2)
+        || currentRPS >= targetRPS - 20.0 / 60.0) {
+      shotRecentlyFired = false;
+    }
+
+    lastRPM = currentRPM;
+    switch (state) {
+      case SPIN_UP:
+        double voltage = setShooterRPM(targetRPS, currentRPS, spinUpPID);
+        shooterMotor.setControl(voltageRequest.withOutput(voltage));
+        break;
+      case RAPID_FIRE:
+        if (Math.abs(currentRPM - targetRPM) <= rpmRapidFireTolerance) {
+          // feed balls
+        } else {
+          // dont feed
+        }
+        break;
+      case RAPID_FIRE_ACCURATE:
+        if (Math.abs(currentRPM - targetRPM) <= rpmAccurateTolerance) {
+          // feed
+        } else {
+          // no no feed
+        }
+        break;
+      case IDLE:
+      case EMPTY:
+        shooterMotor.setControl(voltageRequest.withOutput(0.0));
+        break;
+    }
+    System.out.println(currentRPM + " " + shotRecentlyFired);
+    //    System.out.println(
+    //        "Shooter State: "
+    //            + state
+    //            + " Target RPM: "
+    //            + targetRPM
+    //            + " Current RPM: "
+    //            //            + currentRPM
+    //            + " Voltage Commanded: "
+    //            + setShooterRPM(targetRPS, currentRPS, spinUpPID)
+    //            + " Shot Recently Fired: "
+    //            + shotRecentlyFired
+    //            + " Atspeed: "
+    //            + isAtspeed(targetRPS, currentRPS, rpmRapidFireTolerance));
+  }
+
+  double getVelocityRevPerSec() {
+    double motorRPS = shooterMotor.getVelocity().getValueAsDouble();
+    return (motorRPS / GEAR_RATIO);
+  }
+
+  public double setShooterRPM(double targetRPS, double currentRPS, PIDController PID) {
+    //    double accelleration = (targetRPS - lastTargetRPS) / 0.02;
+    if (shotRecentlyFired && currentRPS < targetRPS - 50.0 / 60.0) {
+      PID.setP(kP_BOOST);
+      PID.setD(kD_BOOST);
+      System.out.println("BOOST");
+    } else {
+      PID.setP(kP_SPIN_UP);
+      PID.setD(kD_SPIN_UP);
+    }
+
+    double ffVolts = feedforward.calculate(targetRPS);
+    // maybe add deadband here for osolation?
+    double pidVolts = PID.calculate(currentRPS, targetRPS);
+    double output = ffVolts + pidVolts;
+
+    lastTargetRPS = targetRPS;
+    output = MathUtil.clamp(output, -12.0, 12.0);
+    return output;
+  }
+
+  public void setState(ShooterState newState) {
+    state = newState;
+  }
+
+  private double getTargetDistance() {
+    // find distance somehow idk prob pose
+    return 2.0;
+  }
+
+  private boolean isAtSpeed(double targetRPS, double currentRPS, double toleranceRM) {
+    return (Math.abs(currentRPS * 60.0 - targetRPS * 60.0) <= toleranceRM);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/turret/turrettestingSubsystem.java
@@ -0,0 +1,420 @@
+package frc.robot.subsystems.turret;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.Supplier;
+// TODO: start making it target a pose not limelight, make limelight update the pose, but reject bad
+// data thats too far away, start making shooting on the move
+// TODO: merge this with advantagekit talonfx swerve code
+
+public class turrettestingSubsystem extends SubsystemBase {
+
+  //  private static final turrettestingSubsystem INSTANCE = new turrettestingSubsystem();
+  //
+  //  public static turrettestingSubsystem getInstance() {
+  //    return INSTANCE;
+  //  }
+
+  private static double VISION_LATENCY;
+
+  private final NetworkTable limelightTable;
+  private final Translation2d HUB_POSE =
+      new Translation2d(Units.inchesToMeters(182.11), Units.inchesToMeters(158.84));
+  private static final double VISION_KP = 0.015;
+  Pose2d robotPose;
+  private final Supplier<Pose2d> poseSupplier;
+  private final Supplier<ChassisSpeeds> chassisSpeedsSupplier;
+
+  private Translation2d robotFieldVelocity;
+
+  private static final Translation2d robotToTurret = new Translation2d(0.35, 0.10);
+
+  private final TalonFX turretMotor = new TalonFX(3);
+  private final VoltageOut voltageRequest = new VoltageOut(0);
+
+  private static final double MAX_ANGLE = 720;
+  private static final double MIN_ANGLE = -720;
+  private static final double MAX_VELOCITY_IN_DEG_PER_SEC = 60;
+  private static final double MAX_ACCELERATION_IN_DEG_PER_SEC = 120;
+  private static final double UNWIND_THRESHOLD = 500;
+  private static final Rotation2d UNWIND_TARGET = Rotation2d.fromDegrees(0.0);
+  private static final double GEAR_RATIO = 60.8; // 1:1
+
+  private final double kP = 2.0; // 2
+  private final double kI = 0.1;
+  private final double kD = 0.1; // 0.1
+  private final double kV = 0.12; // 0.12
+  private final double kA = 0.02; // 0.02
+
+  private final TrapezoidProfile profile;
+  private TrapezoidProfile.State lastSetpoint;
+
+  private Rotation2d targetAngle;
+  private boolean isUnwinding;
+
+  private boolean wrappingAround;
+
+  private double lastTimeAtGoal = 0;
+  private double lastTimestamp = Timer.getFPGATimestamp();
+
+  private static final InterpolatingDoubleTreeMap ballFlightTimeMap =
+      new InterpolatingDoubleTreeMap();
+
+  static {
+    // this is example later replace
+    ballFlightTimeMap.put(2.0, 0.42); // distance in meters, time in seconds
+  }
+
+  public turrettestingSubsystem(
+      Supplier<Pose2d> poseSupplier, Supplier<ChassisSpeeds> chassisSpeedsSupplier) {
+    this.poseSupplier = poseSupplier;
+    this.chassisSpeedsSupplier = chassisSpeedsSupplier;
+    limelightTable = NetworkTableInstance.getDefault().getTable("limelight-three");
+    this.profile =
+        new TrapezoidProfile(
+            new TrapezoidProfile.Constraints(
+                Math.toRadians(MAX_VELOCITY_IN_DEG_PER_SEC),
+                Math.toRadians(MAX_ACCELERATION_IN_DEG_PER_SEC)));
+
+    this.lastSetpoint = new TrapezoidProfile.State(0.0, 0.0);
+    this.targetAngle = new Rotation2d();
+    this.isUnwinding = false;
+
+    var config = new TalonFXConfiguration();
+    config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    config.CurrentLimits.SupplyCurrentLimit = 40.0;
+    config.CurrentLimits.SupplyCurrentLimitEnable = true;
+    config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+    turretMotor.getConfigurator().apply(config);
+    turretMotor.setPosition(0);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    VISION_LATENCY =
+        (limelightTable.getEntry("tl").getDouble(0.0)
+                + limelightTable.getEntry("cl").getDouble(0.0))
+            / 1000.0;
+    robotPose = poseSupplier.get();
+    robotFieldVelocity =
+        new Translation2d(
+                chassisSpeedsSupplier.get().vxMetersPerSecond,
+                chassisSpeedsSupplier.get().vyMetersPerSecond)
+            .rotateBy(robotPose.getRotation());
+
+    double currentPositionDeg = getAbsolutePositionDeg();
+    double currentVelocityDegPerSec = getVelocityDegPerSec();
+
+    double voltage = 0.0;
+
+    if (Math.abs(currentPositionDeg) > UNWIND_THRESHOLD) {
+      isUnwinding = true;
+      targetAngle = UNWIND_TARGET;
+      //      System.out.println("unwinding");
+    } else if (isUnwinding && atGoal(currentPositionDeg)) {
+      // Finished unwinding
+      //      System.out.println(
+      //
+      // "DONEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+      isUnwinding = false;
+    } else if (wrappingAround && atGoal(currentPositionDeg)) {
+      wrappingAround = false;
+    }
+    if (!isUnwinding && !wrappingAround) {
+      targetAngle =
+          findBestTarget(
+              Rotation2d.fromRadians(
+                  calculateTurretgoalRad(
+                      robotPose,
+                      robotFieldVelocity,
+                      chassisSpeedsSupplier.get().omegaRadiansPerSecond)),
+              currentPositionDeg);
+      //      System.out.println("targeting");
+    }
+    if (isUnwinding) {
+      System.out.println("is unwinding");
+      targetAngle = UNWIND_TARGET;
+    }
+
+    //    System.out.println(targetAngle + "" + atGoal(currentPositionDeg));
+    voltage = calculate(currentPositionDeg, currentVelocityDegPerSec, 0.02);
+    turretMotor.setControl(voltageRequest.withOutput(voltage));
+  }
+
+  public void setTarget(Pose2d robotPose, Translation2d robotVelocity, double robotOmegaRadPerSec) {
+    double tx = limelightTable.getEntry("tx").getDouble(0.0);
+    double tv = limelightTable.getEntry("tv").getDouble(0.0);
+
+    //    if (tv < 1) {
+    //      //      System.out.print("no target");
+    //
+    //      //      turretMotor.setControl(voltageRequest.withOutput(0.0));
+    //      //      targetAngle = Rotation2d.fromDegrees(getAbsolutePositionDeg());
+    //      targetAngle = targetAngle; // hold position
+    //      return;
+    //    }
+    Translation2d turretPose =
+        robotPose.getTranslation().plus(robotToTurret.rotateBy(robotPose.getRotation()));
+
+    Translation2d toHub = HUB_POSE.minus(turretPose);
+    // field relative angle
+    double fieldAngle = Math.atan2(toHub.getY(), toHub.getX());
+
+    double desiredTurretAngle =
+        MathUtil.angleModulus(fieldAngle - robotPose.getRotation().getRadians());
+
+    //    // see if we need to unwind
+    //    if (Math.abs(turretMotor.getPosition().getValueAsDouble()) > UNWIND_THRESHOLD) {
+    //      // need to unwind
+    //      isUnwinding = true;
+    //      targetAngle = UNWIND_TARGET;
+    //    } else {
+    // it ok no need to unwind
+    //    isUnwinding = false;
+    //    System.out.print("has target");
+    targetAngle = Rotation2d.fromRadians(desiredTurretAngle);
+
+    //            findBestTarget(
+    //            Rotation2d.fromRadians(desiredTurretAngle),
+    //            getAbsolutePositionDeg()
+    //            );
+
+    //        findBestTarget(
+    //            Rotation2d.fromDegrees(tx + getAbsolutePositionDeg()),
+    //            getAbsolutePositionDeg()); // target from vision
+
+    //    }
+  }
+
+  private Rotation2d findBestTarget(Rotation2d fieldRelativeTarget, double currentAbsoluteDeg) {
+    double wrappedTargetDeg = fieldRelativeTarget.getDegrees();
+
+    double[] canididates = {
+      wrappedTargetDeg,
+      wrappedTargetDeg + 360,
+      wrappedTargetDeg - 360,
+      wrappedTargetDeg + 720,
+      wrappedTargetDeg - 720
+    };
+
+    double bestTarget = currentAbsoluteDeg;
+    double smallestError = Double.POSITIVE_INFINITY;
+
+    for (double candidate : canididates) {
+      if (candidate < MIN_ANGLE || candidate > MAX_ANGLE) {
+        continue;
+      }
+
+      double error = Math.abs(candidate - currentAbsoluteDeg);
+
+      if (error < smallestError) {
+        smallestError = error;
+        bestTarget = candidate;
+      }
+    }
+    //    System.out.println(smallestError);   change back
+    if (smallestError > 300) {
+      lastTimeAtGoal = Timer.getFPGATimestamp();
+      wrappingAround = true;
+    }
+    return Rotation2d.fromDegrees(bestTarget);
+  }
+
+  public double calculate(
+      double currentAngleDeg, double currentVelocityDegPerSec, double deltaTimeSec) {
+    //    currentAngleDeg = MathUtil.clamp(currentAngleDeg, MIN_ANGLE, MAX_ANGLE); // safe it or
+    // smth
+
+    double now = Timer.getFPGATimestamp();
+    double dt = now - lastTimestamp;
+    lastTimestamp = now;
+
+    if (dt <= 0) dt = 0.02;
+
+    targetAngle =
+        Rotation2d.fromDegrees(MathUtil.clamp(targetAngle.getDegrees(), MIN_ANGLE, MAX_ANGLE));
+    TrapezoidProfile.State goal = new TrapezoidProfile.State(targetAngle.getRadians(), 0.0);
+
+    TrapezoidProfile.State setpoint = profile.calculate(dt, lastSetpoint, goal);
+
+    double velocityError = setpoint.velocity - Math.toRadians(currentVelocityDegPerSec);
+    double positionError = setpoint.position - Math.toRadians(currentAngleDeg);
+
+    double accelleration = (setpoint.velocity - lastSetpoint.velocity) / dt;
+    System.out.println(
+        "position error: "
+            + Math.toDegrees(positionError)
+            + " velocity error: "
+            + Math.toDegrees(velocityError)
+            + " setpoint vel: "
+            + Math.toDegrees(setpoint.velocity)
+            + " acceleration: "
+            + Math.toDegrees(accelleration));
+    double output =
+        kP * positionError + kD * velocityError + kV * setpoint.velocity + kA * accelleration;
+    output = MathUtil.clamp(output, -12.0, 12.0);
+
+    lastSetpoint = setpoint;
+
+    return output;
+
+    //    targetAngle =
+    //        Rotation2d.fromDegrees(MathUtil.clamp(targetAngle.getDegrees(), MIN_ANGLE,
+    // MAX_ANGLE));
+    //
+    //    // Simple P control (no motion profile for now)
+    //    double output = 0;
+    //    double positionError = targetAngle.getDegrees() - currentAngleDeg;
+    //    if (wrappingAround) {
+    //      double timeNotAtGoal = Timer.getFPGATimestamp() - lastTimeAtGoal;
+    //      output = kP * positionError + timeNotAtGoal * kI;
+    //      System.out.println(timeNotAtGoal * kI + "intergral");
+    //    } else {
+    //      output = kP * positionError;
+    //    }
+    //    output = MathUtil.clamp(output, -12.0, 12.0);
+    //
+    //    //    System.out.println("Error: " + positionError + " Output: " + output);
+    //
+    //    return output;
+  }
+
+  public double getAbsolutePositionDeg() {
+    double motorRotations = turretMotor.getPosition().getValueAsDouble();
+    return (motorRotations / GEAR_RATIO) * 360.0;
+  }
+
+  public double getVelocityDegPerSec() {
+    double motorRPS = turretMotor.getVelocity().getValueAsDouble();
+    return (motorRPS / GEAR_RATIO) * 360.0;
+  }
+
+  public double applyAngularLead(
+      double turretAngleRad, double robotOmegaRadPerSec, Pose2d robotPose) {
+    double totalTime;
+    if (isVisionTrustworthy(
+        limelightTable.getEntry("tx").getDouble(0.0),
+        Math.toRadians(getVelocityDegPerSec()),
+        robotOmegaRadPerSec,
+        getDistanceFromHub(robotPose))) {
+      //      totalTime = VISION_LATENCY + ballFlightTimeMap.get(getDistanceFromHub(robotPose));
+      totalTime = ballFlightTimeMap.get(getDistanceFromHub(robotPose));
+    } else {
+      totalTime = ballFlightTimeMap.get(getDistanceFromHub(robotPose));
+    }
+    return turretAngleRad + robotOmegaRadPerSec * totalTime;
+  }
+
+  public double applyTranslationalLead(
+      double turretAngleRad,
+      Translation2d robotVelocity,
+      double robotOmegaRadPerSec,
+      Rotation2d robotHeading,
+      Translation2d turretToHub) {
+    Translation2d turretVelocity =
+        robotVelocity.plus(
+            new Translation2d(
+                -robotOmegaRadPerSec * robotToTurret.getY(),
+                robotOmegaRadPerSec * robotToTurret.getX()));
+
+    Translation2d shotDirection = turretToHub.div(turretToHub.getNorm());
+
+    Translation2d lateralDir = new Translation2d(-shotDirection.getY(), shotDirection.getX());
+
+    double distance = turretToHub.getNorm();
+    double lateralVelocity =
+        turretVelocity.getX() * lateralDir.getX() + turretVelocity.getY() * lateralDir.getY();
+
+    double lead = Math.atan2(lateralVelocity * ballFlightTimeMap.get(distance), distance);
+
+    return turretAngleRad + lead;
+  }
+
+  public static double applyVisionCorrection(
+      double turretAngleRad, double txDeg, double turretAngularVelocity) {
+    // if moving fast, ignore correction
+    //    if (Math.abs(turretAngularVelocity) > Math.toRadians(120)) {
+    //      return turretAngleRad;
+    //    }
+    // vision nudgey
+    return turretAngleRad
+        + MathUtil.clamp(VISION_KP * Math.toRadians(txDeg), -Math.toRadians(5), Math.toRadians(5));
+  }
+
+  public static boolean isVisionTrustworthy(
+      double tx, double turretOmega, double robotOmega, double distance) {
+    return Math.abs(tx) < 5.0
+        && Math.abs(turretOmega) < Math.toRadians(120)
+        && Math.abs(robotOmega) < Math.toRadians(180)
+        && distance < 6.0;
+  }
+
+  public double getDistanceFromHub(Pose2d robotPose) {
+    Translation2d turretPose =
+        robotPose.getTranslation().plus(robotToTurret.rotateBy(robotPose.getRotation()));
+
+    Translation2d toHub = HUB_POSE.minus(turretPose);
+
+    return toHub.getNorm();
+  }
+
+  public boolean atGoal(double currentPositionDeg) {
+    double positionError = Math.abs(targetAngle.getDegrees() - currentPositionDeg);
+    double velocityError = Math.abs(getVelocityDegPerSec());
+    //    System.out.println(positionError + "   egrnkjnkjrekjgnkj   " + velocityError);
+    if (positionError < 3.0 && velocityError < 3.0) {
+      lastTimeAtGoal = Timer.getFPGATimestamp();
+      wrappingAround = false;
+      return true;
+    }
+    //    return positionError < 25.0 && velocityError < 3.0;
+    return false;
+  }
+
+  public double calculateTurretgoalRad(
+      Pose2d robotPose, Translation2d robotVelocity, double robotOmega) {
+    setTarget(robotPose, robotVelocity, robotOmega);
+    double angle = targetAngle.getRadians();
+    angle = applyAngularLead(angle, robotOmega, robotPose);
+
+    angle =
+        applyTranslationalLead(
+            angle,
+            robotVelocity,
+            robotOmega,
+            robotPose.getRotation(),
+            HUB_POSE.minus(
+                robotPose.getTranslation().plus(robotToTurret.rotateBy(robotPose.getRotation()))));
+
+    if (isVisionTrustworthy(
+        limelightTable.getEntry("tx").getDouble(0.0),
+        Math.toRadians(getVelocityDegPerSec()),
+        robotOmega,
+        getDistanceFromHub(robotPose))) {
+      angle =
+          applyVisionCorrection(
+              angle,
+              limelightTable.getEntry("tx").getDouble(0.0),
+              Math.toRadians(getVelocityDegPerSec()));
+    }
+
+    angle = MathUtil.clamp(angle, Math.toRadians(MIN_ANGLE), Math.toRadians(MAX_ANGLE));
+    return angle;
+  }
+}
