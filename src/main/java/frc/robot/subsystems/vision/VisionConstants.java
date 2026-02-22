a18a2864d187dd23f884c715222af5c58c90a6e2
jamespotato <12345gavinchen@gmail.com>
2026-02-22 00:16:54 -0800

    added AdaptiveAutoCommand and ChaseBallCommand for enhanced autonomous navigation

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -11,6 +11,7 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.util.Units;
 
 public class VisionConstants {
   // AprilTag layout
@@ -18,8 +19,10 @@ public class VisionConstants {
       AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
 
   // Camera names, must match names configured on coprocessor
-  public static String camera0Name = "limelight-three";
-  public static String camera1Name = "camera_1";
+  public static String camera0Name = "limelight-turret";
+  public static String camera1Name = "limelight-left";
+  public static String camera2Name = "limelight-right";
+  public static String camera3Name = "limelight-under";
 
   // Robot to camera transforms
   // (Not used by Limelight, configure in web UI instead)
